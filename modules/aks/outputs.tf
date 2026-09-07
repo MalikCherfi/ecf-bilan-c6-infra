d09850728917d6f694844106899d3f4f7bf86044
malikcherfi <malikcherfi@gmail.com>
2026-09-07 14:31:28 +0200

    Remove unused output for AKS VNet name and format outputs for clarity

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,9 +1,9 @@
+output "aks_vnet_id" {
+  description = "ID de la vnet du cluster AKS"
+  value       = data.azurerm_virtual_network.aks_vnet.id
+}
+
 output "vnet_subnet_id" {
   description = "ID du sous-réseau du cluster AKS"
   value       = data.azurerm_subnet.aks_subnet.id
-}
-
-output "aks_vnet_name" {
-  description = "Le nom de la vnet du cluster AKS"
-  value       = data.azurerm_resources.aks_vnet.name
 }
