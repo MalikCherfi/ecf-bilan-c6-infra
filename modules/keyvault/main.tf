a78ae179dff4562b65d4d85ee55fb730205ba0ba
malikcherfi <malikcherfi@gmail.com>
2026-09-07 16:41:08 +0200

    Update Key Vault configuration to enable RBAC authorization

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -10,6 +10,7 @@ resource "azurerm_key_vault" "storage" {
   resource_group_name           = var.resource_group_name
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = "standard"
+  rbac_authorization_enabled    = false
   enabled_for_disk_encryption   = false
   purge_protection_enabled      = true # Required for CMK encryption
   soft_delete_retention_days    = 90
