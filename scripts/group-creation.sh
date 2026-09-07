8ffaa2f4b399637182445f4134170c4dce13c4aa
malikcherfi <malikcherfi@gmail.com>
2026-09-07 14:51:54 +0200

    Fix group-creation.sh script: update owner assignment command and adjust formatting

--- scripts/group-creation.sh
+++ scripts/group-creation.sh
@@ -1,10 +1,18 @@
+#!/usr/bin/env bash
+
 GROUP_NAME="mcherfi_employes"
 
 MY_USER_ID=$(az ad signed-in-user show --query id --output tsv)
 
 az ad group create \
   --display-name "$GROUP_NAME" \
-  --mail-nickname "$GROUP_NAME" \
-  --owners "$MY_USER_ID"
+  --mail-nickname "$GROUP_NAME"
 
-az ad group owner list --group "$GROUP_NAME" --query "[].{Name:displayName, UserPrincipalName:userPrincipalName}" --output table
+az ad group owner add \
+  --group "$GROUP_NAME" \
+  --owner-object-id "$MY_USER_ID"
+
+az ad group owner list \
+  --group "$GROUP_NAME" \
+  --query "[].{Name:displayName, UserPrincipalName:userPrincipalName}" \
+  --output table
