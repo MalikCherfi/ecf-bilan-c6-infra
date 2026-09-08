1debee1b1fe088f036f7b7a9ddace417ab59235f
malikcherfi <malikcherfi@gmail.com>
2026-09-08 12:21:39 +0200

    Refactor storage account module configuration for improved clarity and consistency

--- main.tf
+++ main.tf
@@ -25,17 +25,18 @@ module "aks" {
 module "storage-account" {
   source = "./modules/storage-account"
 
-  owner                      = "malik-cherfi"
-  resource_group_name        = data.azurerm_resource_group.rg.name
-  node_resource_group_name   = var.node_resource_group_name
-  location                   = data.azurerm_resource_group.rg.location
-  tags                       = local.tags
-  key_vault_access_policy_id = module.keyvault.key_vault_access_policy_id
-  key_vault_key_id           = module.keyvault.key_vault_key_id
-  user_assigned_identity_id  = module.keyvault.user_assigned_identity_id
-  aks_vnet_id                = module.aks.aks_vnet_id
-  aks_subnet_id              = module.aks.vnet_subnet_id
-  employes_group_object_id   = var.employes_group_object_id
+  owner                          = "malik-cherfi"
+  resource_group_name            = data.azurerm_resource_group.rg.name
+  node_resource_group_name       = var.node_resource_group_name
+  location                       = data.azurerm_resource_group.rg.location
+  tags                           = local.tags
+  key_vault_access_policy_id     = module.keyvault.key_vault_access_policy_id
+  key_vault_key_id               = module.keyvault.key_vault_key_id
+  user_assigned_identity_id      = module.keyvault.user_assigned_identity_id
+  aks_vnet_id                    = module.aks.aks_vnet_id
+  aks_subnet_id                  = module.aks.vnet_subnet_id
+  employes_group_object_id       = var.employes_group_object_id
+  aks_kubelet_identity_object_id = module.aks.aks_kubelet_identity_object_id
 
 }
 
