6b94b1be34556867c0878ffe1964d530296b123c
malikcherfi <malikcherfi@gmail.com>
2026-09-08 14:16:42 +0200

    Fix output value for AKS kubelet identity object ID in outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -10,5 +10,5 @@ output "vnet_subnet_id" {
 
 output "aks_kubelet_identity_object_id" {
   description = "Object ID de l'identité du kubelet du cluster AKS"
-  value       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  value       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].principal_id
 }
