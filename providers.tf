fb9223ab9505a3ff127cdfde08bdb802e3209459
malikcherfi <malikcherfi@gmail.com>
2026-09-09 09:58:23 +0200

    Add Kubernetes and Helm provider configurations in providers.tf

--- providers.tf
+++ providers.tf
@@ -10,6 +10,14 @@ terraform {
       source  = "hashicorp/azuread"
       version = "~> 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.2.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.3.0"
+    }
   }
 }
 
@@ -18,5 +26,21 @@ provider "azurerm" {
     key_vault {
       purge_soft_delete_on_destroy = false
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.aks.host
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
   }
 }
