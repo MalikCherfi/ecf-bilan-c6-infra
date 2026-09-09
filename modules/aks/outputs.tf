84696c26972558b4bdaf590d5d69cffa1fbd1550
malikcherfi <malikcherfi@gmail.com>
2026-09-09 09:58:26 +0200

    Add sensitive outputs for AKS kube_config in outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -12,3 +12,23 @@ output "aks_kubelet_identity_principal_id" {
   description = "Principal ID de l'identité du kubelet du cluster AKS"
   value       = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive = true
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
