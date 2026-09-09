2fc2396164b60faaf456657e5ead22b0c418f6b5
malikcherfi <malikcherfi@gmail.com>
2026-09-09 10:05:05 +0200

    Fix namespace reference in Argo CD Helm release configuration to use v1 API

--- argocd.tf
+++ argocd.tf
@@ -8,7 +8,7 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
   version    = "10.8.4"
 
   values = [
