72fd33332b591a1a73bcff5897f801b5ac4ebeae
malikcherfi <malikcherfi@gmail.com>
2026-09-09 09:58:20 +0200

    Add Argo CD Kubernetes namespace and Helm release configuration

---
+++ argocd.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = "10.8.4"
+
+  set = {
+    name  = "server.service.type"
+    value = "ClusterIP"
+  }
+}
